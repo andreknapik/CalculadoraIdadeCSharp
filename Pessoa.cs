86b7f560f128ddf5d38ccc834db79789b5b434b3
Andre Knapik <andreluisknapik@gmail.com>
2022-07-12 09:21:52 -0300

    Finalziação do Exercício

---
+++ Pessoa.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraIdadeCSharp
+{
+    public class Pessoa
+    {
+        private int anoNascimento;
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+            set { this.anoNascimento = value; }
+        }
+
+        private String nome;
+
+        public String Nome
+
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: "+this.Nome);
+            Console.WriteLine("Ano de nascimento "+ this.AnoNascimento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: "+ idade);
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.AnoNascimento;
+            return idade;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculadoraIdadeCSharp;
+
+Console.WriteLine("Programa que calcula a idade de uma pessoa");
+Pessoa p = new Pessoa();
+Console.Write("Nome da pessoa: ");
+p.Nome = Console.ReadLine();
+Console.Write("Ano de Nascimento: ");
+p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("####################################");
+p.ExibirDados();
+Console.ReadKey();
